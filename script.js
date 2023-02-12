f3d20692e5aab1f7fbe7bc6fa91ffdb5db5903f7
JohnLu2004 <john.lu10212004@gmail.com>
2023-02-11 23:10:05 -0500

    making pushes

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction(){
+    //send backend a request
+
+    //get info about percentage of peeps that like it
+
+    //change values
+    document.getElementById("percent").innerText = "30%";
+    document.getElementById("percent").style.width = "50%";
+    console.log("works");
+}
