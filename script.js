ad6765fde8b0e4c55f7ebd0c94805bbe2f0275ab
JohnLu2004 <john.lu10212004@gmail.com>
2023-02-12 00:10:45 -0500

    getting closer

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-function myFunction(){
-    //send backend a request
 
-    //get info about percentage of peeps that like it
+var butt = document.getElementById("myButton");
+if(butt){
+    butt.addEventListener("click", function(){
+        let url = getCurrentTab();
+        console.log(url);
+        
+        //send backend a request
+        let data = fetchAsync (url);
+    
+        //get info about percentage of peeps that like it
+    
+        //change values
+        document.getElementById("percent").innerText = "30%";
+        document.getElementById("percent").style.width = "50%";
+    });
+}else{
+    console.log("it don't exist");
+}
 
-    //change values
-    document.getElementById("percent").innerText = "30%";
-    document.getElementById("percent").style.width = "50%";
-    console.log("works");
+
+async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+}
+
+async function fetchAsync (url) {
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
 }
